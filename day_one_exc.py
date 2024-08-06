5661084cbf7228a73cc63b5af22f8d7643a9c222
T-Mabe <abemolina03@gmail.com>
2024-08-05 22:02:59 -0400

    Day 1 exercises

---
+++ day_one_exc.py
@@ -0,0 +1,35 @@
+# exercise 1
+print("Exercise 1")
+text = input("Enter some text: ")
+print(text)
+
+# exercise 2
+print("Exercise 2")
+number = int(input("Enter number: "))
+print(number + 1)
+
+# exercise 3
+print("Exercise 3")
+number2 = float(input("Enter number: "))
+print(number2 + .5)
+
+# exercise 4
+print("Exercise 4")
+float1 = float(input("Enter a number: "))
+float2 = float(input("Enter another number: "))
+total =  float1 + float2
+print(f"The sum is {total}")
+
+# exercise 5
+print("Exercise 5")
+entry = float(input("Enter a number: "))
+entry2 = float(input("Enter another number: "))
+sum = entry * entry2
+print(f"The product is {sum}")
+
+# exercise 6
+print("Exercise 6")
+integer1 = int(input("Enter a number: "))
+integer2 = int(input("Enter another number: "))
+total2 = integer1 / integer2
+print(total2)
