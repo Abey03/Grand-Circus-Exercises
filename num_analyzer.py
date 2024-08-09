d99771f8130f22e25b5fd711f17b2b9f9df0b242
Abey03 <abemolina03@gmail.com>
2024-08-08 21:03:33 -0400

    Lab 1 Number Analyzer

---
+++ num_analyzer.py
@@ -0,0 +1,19 @@
+num = int(input("Please enter a number: "))
+
+
+if num % 2 != 0 and num < 60:
+        print(num)
+        print("Odd and less than 60")
+elif num % 2 == 0 and num >= 2 and num <= 24:
+        print(num)
+        print("even and less than 25")
+elif num % 2 == 0 and num >= 26 and num <= 60:
+        print(num)
+        print("Even and between 26 and 60 inclusive")
+elif num % 2 == 0 and num > 60:
+        print(num)
+        print("Even and greater than 60")
+elif num % 2 != 0 and num > 60:
+        print(num)
+        print("Odd and greater than 60")
+
